fe20162e849d61de9d85462c325f26034bd3203a
areiz <areiz@github.com>
2018-06-29 13:14:01 -0500

    First commit

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -14,18 +14,18 @@ export class DashboardComponent implements OnInit {
     constructor() {
         this.sliders.push(
             {
-                imagePath: 'assets/images/slider1.jpg',
+                imagePath: 'assets/images/team-meetingimage.jpg',
                 label: 'First slide label',
                 text:
                     'Nulla vitae elit libero, a pharetra augue mollis interdum.'
             },
             {
-                imagePath: 'assets/images/slider2.jpg',
+                imagePath: 'assets/images/dental-image2.jpg',
                 label: 'Second slide label',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
             },
             {
-                imagePath: 'assets/images/slider3.jpg',
+                imagePath: 'assets/images/dental-image3.jpg',
                 label: 'Third slide label',
                 text:
                     'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
